8749585587ca9b34b651cfc7f5667bc87066d801
huangzx41 <302994907@qq.com>
2020-05-06 09:19:38 +0800

    第一次版本

---
+++ oilMail/src/main/java/com/xq/tmall/controller/admin/aa.java
@@ -0,0 +1,12 @@
+package com.xq.tmall.controller.admin;
+
+import cn.hutool.crypto.SecureUtil;
+
+public class aa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println(SecureUtil.sha256("123456"));
+	}
+
+}
